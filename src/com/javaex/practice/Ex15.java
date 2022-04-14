4a421ffc9a611441089368644bec1180040e5a9e
gardenee <snnjjang@naver.com>
2022-04-14 21:33:13 +0900

    220414

--- src/com/javaex/practice/Ex15.java
+++ src/com/javaex/practice/Ex15.java
@@ -7,12 +7,12 @@ public class Ex15 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		final double mile = 1.609;
+		final double Mile = 1.609;
 		
 		System.out.print("마일을 입력하세요: ");
 		int km = sc.nextInt();
 		
-		System.out.println(km + "마일은 " + (km * mile) + "킬로미터입니다.");
+		System.out.println(km + "마일은 " + (km * Mile) + "킬로미터입니다.");
 		
 	}
 
